69a38347976f40d87eceb587a76f379768428a7c
Marcel Baumgartner <marcel.kbkm@gmail.com>
2023-03-18 23:59:36 +0100

    Implemented moonlight report endpoints

---
+++ Moonlight/App/Http/Controllers/Api/Remote/DdosController.cs
@@ -0,0 +1,41 @@
+﻿using Logging.Net;
+using Microsoft.AspNetCore.Mvc;
+using Moonlight.App.Http.Requests.Daemon;
+using Moonlight.App.Repositories;
+using Moonlight.App.Services;
+
+namespace Moonlight.App.Http.Controllers.Api.Remote;
+
+[ApiController]
+[Route("api/remote/ddos")]
+public class DdosController : Controller
+{
+    private readonly NodeRepository NodeRepository;
+    private readonly MessageService MessageService;
+
+    public DdosController(NodeRepository nodeRepository, MessageService messageService)
+    {
+        NodeRepository = nodeRepository;
+        MessageService = messageService;
+    }
+
+    [HttpPost("update")]
+    public async Task<ActionResult> Update([FromBody] DdosStatus ddosStatus)
+    {
+        var tokenData = Request.Headers.Authorization.ToString().Replace("Bearer ", "");
+        var id = tokenData.Split(".")[0];
+        var token = tokenData.Split(".")[1];
+
+        var node = NodeRepository.Get().FirstOrDefault(x => x.TokenId == id);
+
+        if (node == null)
+            return NotFound();
+        
+        if (token != node.Token)
+            return Unauthorized();
+
+        await MessageService.Emit("node.ddos", ddosStatus);
+        
+        return Ok();
+    }
+}
