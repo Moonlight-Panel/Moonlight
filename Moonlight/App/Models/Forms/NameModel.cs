49e75c5a8d9f36fd263e86a5e2f45d15e02f69e6
Daniel Balk <67603460+Daniel-Balk@users.noreply.github.com>
2023-04-03 21:22:54 +0200

    require name before use (--> user status 7)

---
+++ Moonlight/App/Models/Forms/NameModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moonlight.App.Models.Forms;
+
+public class NameModel
+{
+    [Required]
+    [MinLength(2, ErrorMessage = "Do you think, that works?")]
+    public string FirstName { get; set; }
+    
+    [Required]
+    [MinLength(2, ErrorMessage = "Do you think, that works?")]
+    public string LastName { get; set; }
+}
