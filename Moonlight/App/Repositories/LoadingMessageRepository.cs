0e6910f46bff432c0b73c76fc9a78349b5393f4d
Spielepapagei <Spielepapagei@outlook.de>
2023-04-05 15:02:29 +0200

    Added LoadingMessagesRepository
    Migrated LoadingMessages from Moonlight v1

---
+++ Moonlight/App/Repositories/LoadingMessageRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moonlight.App.Database;
+using Moonlight.App.Database.Entities;
+
+namespace Moonlight.App.Repositories;
+
+public class LoadingMessageRepository : IDisposable
+{
+    private readonly DataContext DataContext;
+
+    public LoadingMessageRepository(DataContext dataContext)
+    {
+        DataContext = dataContext;
+    }
+
+    public DbSet<LoadingMessage> Get()
+    {
+        return DataContext.LoadingMessages;
+    }
+
+    public LoadingMessage Add(LoadingMessage loadingMessage)
+    {
+        var x = DataContext.LoadingMessages.Add(loadingMessage);
+        DataContext.SaveChanges();
+        return x.Entity;
+    }
+
+    public void Update(LoadingMessage loadingMessage)
+    {
+        DataContext.LoadingMessages.Update(loadingMessage);
+        DataContext.SaveChanges();
+    }
+
+    public void Delete(LoadingMessage loadingMessage)
+    {
+        DataContext.LoadingMessages.Remove(loadingMessage);
+        DataContext.SaveChanges();
+    }
+    
+    public void Dispose()
+    {
+        DataContext.Dispose();
+    }
+}
