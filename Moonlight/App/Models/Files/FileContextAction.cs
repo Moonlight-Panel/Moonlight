882ab5cf27f18f4d42e6411e4b5b485c34aed8f4
Marcel Baumgartner <marcel.kbkm@gmail.com>
2023-04-13 16:36:28 +0200

    Removed old file manager stuff

--- Moonlight/App/Models/Files/FileContextAction.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Moonlight.App.Models.Files;
-
-public class FileContextAction
-{
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public Action<FileManagerObject> Action { get; set; }
-}
