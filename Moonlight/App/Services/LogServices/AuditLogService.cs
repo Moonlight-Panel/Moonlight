227ad493b734c8d274d56893653f0a7aae58b24c
Daniel Balk <67603460+Daniel-Balk@users.noreply.github.com>
2023-04-14 18:32:50 +0200

    possible audit log  fix

--- Moonlight/App/Services/LogServices/AuditLogService.cs
+++ Moonlight/App/Services/LogServices/AuditLogService.cs
@@ -73,8 +73,11 @@ public class AuditLogService
     {
         private List<LogData> Data = new List<LogData>();
 
-        public void Add<T>(object data)
+        public void Add<T>(object? data)
         {
+            if(data == null)
+                return;
+            
             Data.Add(new LogData()
             {
                 Type = typeof(T),
