2e83b1d82898f2aee55ce13cd1ac53d497f93670
Marcel Baumgartner <marcel.kbkm@gmail.com>
2023-04-16 00:16:58 +0200

    Optimized interop client side

--- Moonlight/wwwroot/assets/js/recaptcha.js
+++
@@ -1,9 +0,0 @@
-﻿window.recaptcha = new Object();
-window.recaptcha.render = function (id, sitekey, page)
-{
-    return grecaptcha.render(id, {
-        'sitekey': sitekey,
-        'callback': (response) => { page.invokeMethodAsync('CallbackOnSuccess', response); },
-        'expired-callback': () => { page.invokeMethodAsync('CallbackOnExpired'); }
-    });
-}
