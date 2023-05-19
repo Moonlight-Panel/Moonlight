306ad51a51861c03bf054992850ed671b4e22035
Marcel Baumgartner <marcel.kbkm@gmail.com>
2023-05-19 14:36:51 +0200

    Added recaptcha. Added recaptcha to register page

---
+++ Moonlight/App/Helpers/MustBeTrueAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Logging.Net;
+
+namespace Moonlight.App.Helpers;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class MustBeTrueAttribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        if (value is bool boolValue)
+        {
+            return boolValue;
+        }
+
+        return false;
+    }
+}
--- Moonlight/App/Models/Forms/UserRegisterModel.cs
+++ Moonlight/App/Models/Forms/UserRegisterModel.cs
@@ -1,21 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Moonlight.App.Helpers;
 
 namespace Moonlight.App.Models.Forms;
 
 public class UserRegisterModel
 {
-    [Required, EmailAddress]
+    [Required(ErrorMessage = "You need to specify a email address")]
+    [EmailAddress(ErrorMessage = "You need to specify a valid email address")]
     public string Email { get; set; }
     
-    [Required, MinLength(3)]
+    [Required(ErrorMessage = "You need to specify a first name")]
+    [MinLength(3, ErrorMessage = "You need to specify a valid first name")]
+    [MaxLength(30, ErrorMessage = "You need to specify a valid first name")]
     public string FirstName { get; set; }
     
-    [Required, MinLength(3)]
+    
+    [Required(ErrorMessage = "You need to specify a last name")]
+    [MinLength(3, ErrorMessage = "You need to specify a valid last name")]
+    [MaxLength(30, ErrorMessage = "You need to specify a valid last name")]
     public string LastName { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = "You need to specify a password")]
     public string Password { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = "You need to specify a password")]
     public string ConfirmPassword { get; set; }
+    
+    [MustBeTrue(ErrorMessage = "Please solve the captcha")]
+    public bool Captcha { get; set; }
 }
