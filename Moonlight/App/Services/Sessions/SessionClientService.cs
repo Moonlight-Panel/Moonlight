609d5451f994b736cab54e0b65714273e8ae7063
Marcel Baumgartner <marcel.kbkm@gmail.com>
2023-07-04 17:49:27 +0200

    Rewritten session system to match new standarts and be more performant

---
+++ Moonlight/App/Services/Sessions/SessionClientService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Moonlight.App.Database.Entities;
+using Moonlight.App.Repositories;
+using Moonlight.App.Services.Interop;
+
+namespace Moonlight.App.Services.Sessions;
+
+public class SessionClientService
+{
+    public readonly Guid Uuid = Guid.NewGuid();
+    public readonly DateTime CreateTimestamp = DateTime.UtcNow;
+    public User? User { get; private set; }
+
+    public readonly IdentityService IdentityService;
+    public readonly AlertService AlertService;
+    public readonly NavigationManager NavigationManager;
+    public readonly IJSRuntime JsRuntime;
+
+    private readonly SessionServerService SessionServerService;
+    private readonly Repository<User> UserRepository;
+
+    public SessionClientService(
+        IdentityService identityService,
+        AlertService alertService,
+        NavigationManager navigationManager,
+        IJSRuntime jsRuntime,
+        SessionServerService sessionServerService,
+        Repository<User> userRepository)
+    {
+        IdentityService = identityService;
+        AlertService = alertService;
+        NavigationManager = navigationManager;
+        JsRuntime = jsRuntime;
+        SessionServerService = sessionServerService;
+        UserRepository = userRepository;
+    }
+
+    public async Task Start()
+    {
+        User = await IdentityService.Get();
+
+        if (User != null) // Track users last visit
+        {
+            User.LastVisitedAt = DateTime.UtcNow;
+            UserRepository.Update(User);
+        }
+
+        await SessionServerService.Register(this);
+    }
+
+    public async Task Stop()
+    {
+        await SessionServerService.UnRegister(this);
+    }
+}
