f52b9e295138dbb423b2728846f107109c2d8ef7
Marcel Baumgartner <marcel.kbkm@gmail.com>
2023-07-07 03:06:16 +0200

    Added telemetry reporter

---
+++ Moonlight/App/ApiClients/Telemetry/TelemetryException.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+
+namespace Moonlight.App.ApiClients.Telemetry;
+
+[Serializable]
+public class TelemetryException : Exception
+{
+    public int StatusCode { get; set; }
+
+    public TelemetryException()
+    {
+    }
+    
+    public TelemetryException(string message, int statusCode) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+
+    public TelemetryException(string message) : base(message)
+    {
+    }
+
+    public TelemetryException(string message, Exception inner) : base(message, inner)
+    {
+    }
+
+    protected TelemetryException(
+        SerializationInfo info,
+        StreamingContext context) : base(info, context)
+    {
+    }
+}
