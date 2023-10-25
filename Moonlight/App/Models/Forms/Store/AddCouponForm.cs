b0d9837256ed8cf3822ea4a322a3deba5d4ce653
Baumgartner Marcel <marcel.kbkm@gmail.com>
2023-10-25 16:26:42 +0200

    Added auto form stuff and started implementing auto crud and store admin things

---
+++ Moonlight/App/Models/Forms/Store/AddCouponForm.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moonlight.App.Models.Forms.Store;
+
+public class AddCouponForm
+{
+    [MinLength(5, ErrorMessage = "The code needs to be longer than 4")]
+    [MaxLength(15, ErrorMessage = "The code should not be longer than 15 characters")]
+    public string Code { get; set; } = "";
+    
+    [Range(1, 99, ErrorMessage = "The percent needs to be between 1 and 99")]
+    public int Percent { get; set; }
+    
+    [Range(0, int.MaxValue, ErrorMessage = "The amount needs to be equals or greater than 0")]
+    public int Amount { get; set; }
+}
--- Moonlight/App/Models/Forms/Store/AddProductForm.cs
+++ Moonlight/App/Models/Forms/Store/AddProductForm.cs
@@ -1,15 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using Moonlight.App.Database.Entities.Store;
 using Moonlight.App.Database.Enums;
+using Moonlight.App.Extensions.Attributes;
 
 namespace Moonlight.App.Models.Forms.Store;
 
 public class AddProductForm
 {
     [Required(ErrorMessage = "You need to specify a category")]
+    [Selector(DisplayProp = "Name", SelectorProp = "Name", UseDropdown = true)]
     public Category Category { get; set; }
 
     [Required(ErrorMessage = "You need to specify a name")]
+    [Description("Teeeeeeeeeeeeeeeeeeeeeeeeeest")]
     public string Name { get; set; } = "";
     
     [Required(ErrorMessage = "You need to specify a description")]
@@ -19,16 +23,16 @@ public class AddProductForm
     [RegularExpression("^[a-z0-9-]+$", ErrorMessage = "You need to enter a valid slug only containing lowercase characters and numbers")]
     public string Slug { get; set; } = "";
     
-    [Range(0, double.MaxValue, ErrorMessage = "The price needs to be equals or above 0")]
+    [Range(0, double.MaxValue, ErrorMessage = "The price needs to be equals or greater than 0")]
     public double Price { get; set; }
     
-    [Range(0, double.MaxValue, ErrorMessage = "The stock needs to be equals or above 0")]
+    [Range(0, double.MaxValue, ErrorMessage = "The stock needs to be equals or greater than 0")]
     public int Stock { get; set; }
     
-    [Range(0, double.MaxValue, ErrorMessage = "The max per user amount needs to be equals or above 0")]
+    [Range(0, double.MaxValue, ErrorMessage = "The max per user amount needs to be equals or greater than 0")]
     public int MaxPerUser { get; set; }
     
-    [Range(0, double.MaxValue, ErrorMessage = "The duration needs to be equals or above 0")]
+    [Range(0, double.MaxValue, ErrorMessage = "The duration needs to be equals or greater than 0")]
     public int Duration { get; set; }
     
     public ServiceType Type { get; set; }
