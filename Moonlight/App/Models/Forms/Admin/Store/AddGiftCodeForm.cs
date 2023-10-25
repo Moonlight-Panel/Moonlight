7a3d61c6596fd53e5487dd2b945d0b6883094232
Marcel Baumgartner <marcel.kbkm@gmail.com>
2023-10-25 22:22:32 +0200

    FInished auto crud. Switched to auto crud for coupons. Added gift code manage

--- Moonlight/App/Models/Forms/Admin/Store/AddGiftCodeForm.cs
+++ Moonlight/App/Models/Forms/Admin/Store/AddGiftCodeForm.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Moonlight.App.Models.Forms.Store;
+namespace Moonlight.App.Models.Forms.Admin.Store;
 
-public class AddCouponForm
+public class AddGiftCodeForm
 {
     [MinLength(5, ErrorMessage = "The code needs to be longer than 4")]
     [MaxLength(15, ErrorMessage = "The code should not be longer than 15 characters")]
     public string Code { get; set; } = "";
     
-    [Range(1, 99, ErrorMessage = "The percent needs to be between 1 and 99")]
-    public int Percent { get; set; }
+    [Range(0, int.MaxValue, ErrorMessage = "The value needs to be equals or greater than 0")]
+    public double Value { get; set; }
     
     [Range(0, int.MaxValue, ErrorMessage = "The amount needs to be equals or greater than 0")]
     public int Amount { get; set; }
---
+++ Moonlight/App/Models/Forms/Admin/Store/EditGiftCodeForm.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moonlight.App.Models.Forms.Admin.Store;
+
+public class EditGiftCodeForm
+{
+    [MinLength(5, ErrorMessage = "The code needs to be longer than 4")]
+    [MaxLength(15, ErrorMessage = "The code should not be longer than 15 characters")]
+    public string Code { get; set; } = "";
+    
+    [Range(0, int.MaxValue, ErrorMessage = "The value needs to be equals or greater than 0")]
+    public double Value { get; set; }
+    
+    [Range(0, int.MaxValue, ErrorMessage = "The amount needs to be equals or greater than 0")]
+    public int Amount { get; set; }
+}
