801c78bb84ec99e91b3badf407be0398fab41174
Marcel Baumgartner <marcel.kbkm@gmail.com>
2023-10-27 07:34:25 +0200

    Started implementing text editor

--- Moonlight/wwwroot/js/moonlight.js
+++ Moonlight/wwwroot/js/moonlight.js
@@ -105,5 +105,29 @@ window.moonlight = {
 
             return text;
         }
+    },
+    textEditor: {
+        create: function(id)
+        {
+            BalloonEditor
+                .create(document.getElementById(id), {
+                    toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+                    heading: {
+                        options: [
+                            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+                        ]
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        },
+        get: function (id)
+        {
+            let editor = document.getElementById(id).ckeditorInstance;
+            return editor.getData();
+        }
     }
 }
