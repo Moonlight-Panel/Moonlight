46f08059d7b0b282fe4f09b1dbd0de1300501ba6
Marcel Baumgartner <marcel.kbkm@gmail.com>
2024-01-19 10:34:01 +0100

    Implemented transaction dates

--- Moonlight/App/Database/Entities/Store/Transaction.cs
+++ Moonlight/App/Database/Entities/Store/Transaction.cs
@@ -5,4 +5,5 @@ public class Transaction
     public int Id { get; set; }
     public double Price { get; set; }
     public string Text { get; set; } = "";
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
