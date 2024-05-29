6fa91d8890135996bf29dc379a8e2b8eb19b2040
Moritz Deiaco <moritz.deiaco@gmail.com>
2024-05-29 16:27:46 +0200

    Added a Minimum Required Permission Level For the component
    Masu you said it would be possible to handle this in the Components Razor file, but if nothing is shown there will still be an empty space (empty cols are still showing in boostrap, margin on the userdashboard  and admindashboard components). I think this would be the best solution

--- Moonlight/Core/Models/Abstractions/UiComponent.cs
+++ Moonlight/Core/Models/Abstractions/UiComponent.cs
@@ -7,4 +7,6 @@ public class UiComponent
     public required RenderFragment Component { get; set; }
 
     public int Index { get; set; } = 0;
+
+    public int RequiredPermissionLevel { get; set; } = 0;
 }
