cbcc99b6e8781861809c6b12be4f87118c256402
Masu Baumgartner <marcel.kbkm@gmail.com>
2024-06-04 16:36:19 +0200

    Doing some testing with the new auto crud

--- Moonlight/Features/Servers/Models/Forms/Admin/Servers/CreateServerForm.cs
+++ Moonlight/Features/Servers/Models/Forms/Admin/Servers/CreateServerForm.cs
@@ -9,42 +9,50 @@ namespace Moonlight.Features.Servers.Models.Forms.Admin.Servers;
 public class CreateServerForm
 {
     [Required(ErrorMessage = "You need to specify a name")]
+    [Page("General")]
     public string Name { get; set; }
     
     [Required(ErrorMessage = "You need to specify a server owner")]
     [Selector(SelectorProp = "Username", DisplayProp = "Username", UseDropdown = true)]
+    [Page("General")]
     public User Owner { get; set; }
     
     [Required(ErrorMessage = "You need to specify a server image")]
     [Selector(SelectorProp = "Name", DisplayProp = "Name", UseDropdown = true)]
+    [Page("General")]
     public ServerImage Image { get; set; }
     
     [Range(1, int.MaxValue, ErrorMessage = "Enter a valid cpu value")]
     [Description("The cores the server will be able to use. 100 = 1 Core")]
     [Section("Resources", Icon = "bxs-chip")]
+    [Page("General")]
     public int Cpu { get; set; }
     
     [Range(1, int.MaxValue, ErrorMessage = "Enter a valid memory value")]
     [Description("The amount of memory this server will be able to use")]
     [ByteSize(MinimumUnit = 1, Converter = 1, DefaultUnit = 2)]
     [Section("Resources", Icon = "bxs-chip")]
+    [Page("General")]
     public int Memory { get; set; }
     
     [Range(1, int.MaxValue, ErrorMessage = "Enter a valid disk value")]
     [Description("The amount of disk space this server will be able to use")]
     [ByteSize(MinimumUnit = 1, Converter = 1, DefaultUnit = 2)]
     [Section("Resources", Icon = "bxs-chip")]
+    [Page("General")]
     public int Disk { get; set; }
     
     [Description("Whether to use a virtual disk for storing server files. Dont use this if you want to overallocate as the virtual disks will fill out the space you allocate")]
     [Section("Deployment", Icon = "bx-cube")]
     [RadioButtonBool("Virtual Disk", "Simple Volume", TrueIcon = "bxs-hdd", FalseIcon = "bxs-data")]
     [DisplayName("Storage")]
+    [Page("Advanced")]
     public bool UseVirtualDisk { get; set; }
     
     [Required(ErrorMessage = "You need to specify a server node")]
     [Selector(SelectorProp = "Name", DisplayProp = "Name", UseDropdown = true)]
     [Section("Deployment", Icon = "bx-cube")]
+    [Page("General")]
     public ServerNode Node { get; set; }
 
     [Description("The allocations the server should have")]
@@ -52,5 +60,6 @@ public class CreateServerForm
     [Section("Deployment", Icon = "bx-cube")]
     [CustomItemLoader("FreeAllocations")]
     [CustomDisplayFunction("AllocationWithIp")]
+    [Page("Advanced")]
     public List<ServerAllocation> Allocations { get; set; } = new();
 }
