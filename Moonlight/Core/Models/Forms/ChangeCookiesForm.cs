87f93b3caba040a6c555df985ab089f20b67ca1c
Moritz Deiaco <moritz.deiaco@gmail.com>
2024-06-08 12:02:00 +0200

    Added Cookie Settings option on the account settings

---
+++ Moonlight/Core/Models/Forms/ChangeCookiesForm.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace Moonlight.Core.Models.Forms;
+
+public class ChangeCookiesForm
+{
+    [Description("This specifies if you would like to personalize your experience with optional cookies.")]
+    public bool UseOptionalCookies { get; set; } = false;
+}
