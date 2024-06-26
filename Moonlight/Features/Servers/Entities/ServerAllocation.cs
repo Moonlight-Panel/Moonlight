53509ecf63ff9ae58f7679b8d03c19837fbe9861
Marcel Baumgartner <marcel.kbkm@gmail.com>
2024-06-26 10:59:14 +0200

    Fixed smaller issues. Upgraded server node ui

--- Moonlight/Features/Servers/Entities/ServerAllocation.cs
+++ Moonlight/Features/Servers/Entities/ServerAllocation.cs
@@ -3,7 +3,7 @@ namespace Moonlight.Features.Servers.Entities;
 public class ServerAllocation
 {
     public int Id { get; set; }
-    public string IpAddress { get; set; } = "";
+    public string IpAddress { get; set; } = "0.0.0.0";
     public int Port { get; set; }
     public string Note { get; set; } = "";
 }
