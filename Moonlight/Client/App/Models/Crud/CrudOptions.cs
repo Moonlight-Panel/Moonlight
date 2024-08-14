1424dfaca6f8640cf1782c6454262f59ab714639
Baumgartner Marcel <marcel.kbkm@gmail.com>
2024-08-14 17:01:18 +0200

    Started smart crud component

---
+++ Moonlight/Client/App/Models/Crud/CrudOptions.cs
@@ -0,0 +1,16 @@
+using Moonlight.Client.App.Models.Forms;
+using Moonlight.Shared.Http.Resources;
+
+namespace Moonlight.Client.App.Models.Crud;
+
+public class CrudOptions<TItem, TCreateForm, TUpdateForm>
+{
+    public Func<int, int, Task<PagedResponse<TItem>>>? Loader { get; set; }
+    public Func<string, int, int, Task<PagedResponse<TItem>>>? LoaderWithSearch { get; set; }
+    public Func<TCreateForm, Task> CreateFunction { get; set; }
+    public Func<TUpdateForm, Task> UpdateFunction { get; set; }
+    public Func<TItem, Task> DeleteFunction { get; set; }
+
+    public Action<SmartFormOption<TCreateForm>> OnConfigureCreate { get; set; }
+    public Action<SmartFormOption<TUpdateForm>> OnConfigureUpdate { get; set; }
+}
