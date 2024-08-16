e3ef389d32b2254ab83597e2189b50e06e2a7356
Baumgartner Marcel <marcel.kbkm@gmail.com>
2024-08-16 15:43:54 +0200

    Added danger confirm modal. Improved components

--- Moonlight/Client/App/Models/Crud/CrudOptions.cs
+++ Moonlight/Client/App/Models/Crud/CrudOptions.cs
@@ -8,7 +8,7 @@ public class CrudOptions<TItem, TCreateForm, TUpdateForm>
     public Func<int, int, Task<PagedResponse<TItem>>>? Loader { get; set; }
     public Func<string, int, int, Task<PagedResponse<TItem>>>? LoaderWithSearch { get; set; }
     public Func<TCreateForm, Task> CreateFunction { get; set; }
-    public Func<TUpdateForm, Task> UpdateFunction { get; set; }
+    public Func<TUpdateForm, TItem, Task> UpdateFunction { get; set; }
     public Func<TItem, Task> DeleteFunction { get; set; }
 
     public Action<SmartFormOption<TCreateForm>> OnConfigureCreate { get; set; }
--- Moonlight/Client/App/Services/AlertService.cs
+++ Moonlight/Client/App/Services/AlertService.cs
@@ -19,6 +19,18 @@ public class AlertService
     public Task Warning(string title, string text) => LaunchInternal<WarningAlert>(title, text);
     public Task Error(string title, string text) => LaunchInternal<ErrorAlert>(title, text);
 
+    public async Task DangerConfirm(string text, Func<Task> onConfirm, string? icon = null)
+    {
+        await ModalService.Launch<DangerConfirmAlert>(buildAttributes: parameters =>
+        {
+            if(!string.IsNullOrEmpty(icon))
+                parameters.Add("Icon", icon);
+            
+            parameters.Add("Text", text);
+            parameters.Add("OnConfirm", onConfirm);
+        });
+    }
+
     private async Task LaunchInternal<T>(string title, string text) where T : BaseModal
     {
         await ModalService.Launch<T>(buildAttributes: parameters =>
