f48e5d4b19690f90f31f3bc2976736cd760be903
Masu Baumgartner <68913099+Masu-Baumgartner@users.noreply.github.com>
2024-10-06 20:44:18 +0200

    Implemented admin crud ui for users page. Fixed some smaller issues

--- Moonlight.ApiServer/Http/Controllers/Admin/Users/UsersController.cs
+++ Moonlight.ApiServer/Http/Controllers/Admin/Users/UsersController.cs
@@ -5,6 +5,7 @@ using MoonCore.Extended.Helpers;
 using MoonCore.Models;
 using Moonlight.ApiServer.Database.Entities;
 using Moonlight.Shared.Http.Requests.Admin.Users;
+using Moonlight.Shared.Http.Responses.Admin.Users;
 
 namespace Moonlight.ApiServer.Http.Controllers.Admin.Users;
 
@@ -12,25 +13,25 @@ namespace Moonlight.ApiServer.Http.Controllers.Admin.Users;
 [Route("api/admin/users")]
 public class UsersController : Controller
 {
-    private readonly CrudHelper<User> CrudHelper;
+    private readonly CrudHelper<User, UserDetailResponse> CrudHelper;
     private readonly DatabaseRepository<User> UserRepository;
 
-    public UsersController(CrudHelper<User> crudHelper, DatabaseRepository<User> userRepository)
+    public UsersController(CrudHelper<User, UserDetailResponse> crudHelper, DatabaseRepository<User> userRepository)
     {
         CrudHelper = crudHelper;
         UserRepository = userRepository;
     }
 
     [HttpGet]
-    public async Task<IPagedData<User>> Get([FromQuery] int page, [FromQuery] int pageSize = 50)
+    public async Task<IPagedData<UserDetailResponse>> Get([FromQuery] int page, [FromQuery] int pageSize = 50)
         => await CrudHelper.Get(page, pageSize);
 
     [HttpGet("{id}")]
-    public async Task<User> GetSingle(int id)
+    public async Task<UserDetailResponse> GetSingle(int id)
         => await CrudHelper.GetSingle(id);
 
     [HttpPost]
-    public async Task<User> Create([FromBody] CreateUserRequest request)
+    public async Task<UserDetailResponse> Create([FromBody] CreateUserRequest request)
     {
         // Reformat values
         request.Username = request.Username.ToLower().Trim();
@@ -49,9 +50,9 @@ public class UsersController : Controller
     }
 
     [HttpPatch("{id}")]
-    public async Task<User> Update([FromRoute] int id, [FromBody] UpdateUserRequest request)
+    public async Task<UserDetailResponse> Update([FromRoute] int id, [FromBody] UpdateUserRequest request)
     {
-        var user = await CrudHelper.GetSingle(id);
+        var user = await CrudHelper.GetSingleModel(id);
         
         // Reformat values
         request.Username = request.Username.ToLower().Trim();
@@ -63,7 +64,14 @@ public class UsersController : Controller
         
         if (UserRepository.Get().Any(x => x.Email == request.Email && x.Id != user.Id))
             throw new HttpApiException("A user with that email address already exists", 400);
-
+        
+        // Perform hashing the password if required
+        if (!string.IsNullOrEmpty(request.Password))
+        {
+            request.Password = HashHelper.Hash(request.Password);
+            user.TokenValidTimestamp = DateTime.UtcNow; // This change will get applied by the crud helper
+        }
+        
         return await CrudHelper.Update(user, request);
     }
 
---
+++ Moonlight.Shared/Http/Responses/Admin/Users/UserDetailResponse.cs
@@ -0,0 +1,8 @@
+namespace Moonlight.Shared.Http.Responses.Admin.Users;
+
+public class UserDetailResponse
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+}
