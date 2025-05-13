0743bad93c96859f6a6873ce7b0f4485ddbb9c5a
mxritzdev <moritz.deiaco@gmail.com>
2025-05-13 15:28:46 +0200

    added ui for the diagnose system

--- Moonlight.Client/UiConstants.cs
+++ Moonlight.Client/UiConstants.cs
@@ -4,12 +4,12 @@ public static class UiConstants
 {
     public static readonly string[] AdminNavNames =
     [
-        "Overview", "Theme", "Files", "Hangfire", "Advanced"
+        "Overview", "Theme", "Files", "Hangfire", "Advanced", "Diagnose"
     ];
 
     public static readonly string[] AdminNavLinks =
     [
         "/admin/system", "/admin/system/theme", "/admin/system/files", "/admin/system/hangfire",
-        "/admin/system/advanced"
+        "/admin/system/advanced", "/admin/system/diagnose"
     ];
 }
